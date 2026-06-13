fd21e2991a70efe5e4828e905d281a1d438bed20
ميران الحديثي <147077732+Meranalhudaithy@users.noreply.github.com>
2026-06-14 00:14:52 +0300

    Configure static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Tells Next.js to generate static HTML/CSS/JS (creates an 'out' folder)
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
